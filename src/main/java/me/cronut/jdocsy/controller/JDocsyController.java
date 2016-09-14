c1b68660af03ba342ab5706690417df3dd836bfa
CroNut <cronut@posteo.de>
2016-09-14 14:51:41 +0000

    added a few fxml files for the gui

---
+++ src/main/java/me/cronut/jdocsy/controller/JDocsyController.java
@@ -0,0 +1,18 @@
+package me.cronut.jdocsy.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Author cronut
+ * Date 14.09.16
+ */
+public class JDocsyController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/me/cronut/jdocsy/view/JDocSyView.java
@@ -0,0 +1,25 @@
+package me.cronut.jdocsy.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Author cronut
+ * Date 14.09.16
+ */
+public class JDocSyView extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(
+                FXMLLoader.load(getClass().getClassLoader().getResource("fxml/jdocsy.fxml")),
+                1400,
+                900);
+
+        stage.setTitle("jDocSy");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
